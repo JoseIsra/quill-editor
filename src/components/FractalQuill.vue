465a143473c20b5536cac79725b0380711c149ed
JoseIsra <israelmyname@gmail.com>
2022-03-09 15:21:04 -0500

    testin prop

--- src/components/FractalQuill.vue
+++ src/components/FractalQuill.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="container">
+    {{ zoidContentProp }}
     <button id="insert-table" class="btn" @click="table.insertTable(3, 3)">
       <i class="fas fa-table"></i>
     </button>
